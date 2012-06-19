5760c05b4607226742f0952c53b24448b8d95fd7
Justin Falcone <stanulam59@gmail.com>
2012-06-19 08:44:04 -0400

    increased tweet count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @tweets = Tweet.where(approved: true).order("id DESC").limit(10)
+    @tweets = Tweet.where(approved: true).order("id DESC").limit(13)
 
     respond_to do |format|
       format.html # show.html.erb
